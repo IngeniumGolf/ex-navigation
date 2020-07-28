88c6d3fb0a1c91eae945034743888ee16487898d
plougsgaard <thomas@plougsgaard.dk>
2020-07-28 14:22:49 +0200

    fix: Support 6.5" iPhones

--- src/utils/isIPhoneX.js
+++ src/utils/isIPhoneX.js
@@ -1,4 +1,7 @@
 import { Platform, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
-const isIPhoneX = Platform.OS === 'ios' && width === 375 && height === 812;
-export default isIPhoneX;
+// https://kapeli.com/cheat_sheets/iOS_Design.docset/Contents/Resources/Documents/index
+// this code is very stupid but.. yeah..
+const isIPhoneX = Platform.OS === 'ios' && width === 375 && height === 812; // 5.8"
+const isIPhone11 = Platform.OS === 'ios' && width === 414 && height === 896; // 6.5"
+export default isIPhoneX || isIPhone11;
